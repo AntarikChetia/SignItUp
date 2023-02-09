f47a2d5a3f74101b35d5d3358bc8f3e1372f41a7
AntarikChetia <121717432+AntarikChetia@users.noreply.github.com>
2023-02-09 12:36:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.post("/failure",function(req,res){
     res.redirect("/");
 });
 
-app.listen(3000,function(req,res){
+app.listen(process.env.PORT|| 3000,function(req,res){
 console.log("Server is running at port 3000");
 });
 
